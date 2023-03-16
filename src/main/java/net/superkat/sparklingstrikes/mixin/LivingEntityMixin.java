af7143beaec92f83e4274ec16110455f2706fab0
SuperkatSays <89557012+Superkat32@users.noreply.github.com>
2023-03-15 22:40:12 -0400

    Changed mixin target
    REFACTOR: Changed mixin target

--- src/main/java/net/superkat/sparklingstrikes/mixin/LivingEntityMixin.java
+++ src/main/java/net/superkat/sparklingstrikes/mixin/LivingEntityMixin.java
@@ -1,8 +1,10 @@
 package net.superkat.sparklingstrikes.mixin;
 
+import net.minecraft.entity.Attackable;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.world.World;
 import net.superkat.sparklingstrikes.SparklingConfig;
@@ -16,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin extends Entity {
+public abstract class LivingEntityMixin extends Entity implements Attackable {
 	@Shadow @Final private static Logger LOGGER;
 
 	public LivingEntityMixin(EntityType<?> type, World world) {
@@ -51,9 +53,9 @@ public abstract class LivingEntityMixin extends Entity {
 //				}
 //		}
 //	}
-	
-	@Inject(method = "handleStatus", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
-	public void hitEvent(byte status, CallbackInfo ci) {
+
+	@Inject(method = "onDamaged", at = @At("HEAD"))
+	public void hitEvent(DamageSource damageSource, CallbackInfo ci) {
 		if(SparklingConfig.spamLog) {
 			LOGGER.info("Entity has been hit! Calling particles...");
 		}
