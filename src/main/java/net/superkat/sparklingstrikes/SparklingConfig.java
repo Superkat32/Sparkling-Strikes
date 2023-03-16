3434a3c08a6b48c2225875101b8ba026720f7f7d
SuperkatSays <89557012+Superkat32@users.noreply.github.com>
2023-03-15 22:54:47 -0400

    Updated config looks
    REFACTOR: Utilized MidnightLib's new categories. There are currently three categories: Primary (particle), Secondary (particle), and Other
    
    Note: The mod has not been tested on multiplayer

--- src/main/java/net/superkat/sparklingstrikes/SparklingConfig.java
+++ src/main/java/net/superkat/sparklingstrikes/SparklingConfig.java
@@ -4,23 +4,23 @@ import eu.midnightdust.lib.config.MidnightConfig;
 
 public class SparklingConfig extends MidnightConfig {
 
-    @Comment(centered = true) public static Comment primaryParticleOptions;
-    @Entry(name = "Rough Particle Amount",isSlider = true,min=1,max=100) public static int particleAmount = 15;
-    @Entry public static ParticleOption particleOption = ParticleOption.SPARKLE;
+    @Comment(centered = true, category = "primary") public static Comment primaryParticleOptions;
+    @Entry(name = "Rough Particle Amount",isSlider = true,min=1,max=100, category = "primary") public static int particleAmount = 15;
+    @Entry(category = "primary") public static ParticleOption particleOption = ParticleOption.SPARKLE;
     public enum ParticleOption {
         SPARKLE, STAR, HEART, FLOWER, FAIRYLIGHT
     }
 
-    @Comment(centered = true) public static Comment secondaryParticleOptions;
-    @Entry public static boolean spawnSecondaryParticle = false;
-    @Entry(name = "Rough Secondary Particle Amount",isSlider=true,min=1,max=100) public static int secondaryParticleAmount = 7;
-    @Entry public static SecondaryParticleOption secondaryParticleOption = SecondaryParticleOption.HEART;
+    @Comment(centered = true, category = "secondary") public static Comment secondaryParticleOptions;
+    @Entry(category = "secondary") public static boolean spawnSecondaryParticle = false;
+    @Entry(name = "Rough Secondary Particle Amount",isSlider=true,min=1,max=100, category = "secondary") public static int secondaryParticleAmount = 7;
+    @Entry(category = "secondary") public static SecondaryParticleOption secondaryParticleOption = SecondaryParticleOption.HEART;
     public enum SecondaryParticleOption {
         SPARKLE, STAR, HEART, FLOWER, FAIRYLIGHT
     }
 
-    @Comment(centered = true) public static Comment otherOptions;
-    @Entry public static boolean modEnabled = true;
-    @Entry public static boolean spamLog = false;
+    @Comment(centered = true, category = "other") public static Comment otherOptions;
+    @Entry(category = "other") public static boolean modEnabled = true;
+    @Entry(category = "other") public static boolean spamLog = false;
 
 }
